877a22c1db00246a4e03da9d1d29f59cc3fcd946
ligerlac <lino.oscar.gerlach@cern.ch>
2022-05-10 00:43:54 +0200

    bugfix: commented out unfinished vectorized version of streamer finder

--- src/utils/streamersearcher.py
+++ src/utils/streamersearcher.py
@@ -5,18 +5,44 @@ __maintainer__ = "Lino Gerlach"
 __email__ = "lino.oscar.gerlach@cern.ch"
 
 import pandas as pd
+from datetime import datetime
+
+
+def get_streamer_intervals(df):
+    streamer_intervals = []
+    df = df.loc[df['nvolt'] * df['ncurr'] != 0]
+    streamer_on = False
+    for row in df.itertuples(index=True, name='Pandas'):
+        if streamer_on:
+            if row.stable:
+                streamer_on = False
+                b = row.Index + pd.Timedelta(2, "s")
+                if len(streamer_intervals) != 0 and streamer_intervals[-1][1] > a:
+                    streamer_intervals[-1][1] = b
+                else:
+                    streamer_intervals.append([a, b])
+        else:
+            if not row.stable:
+                streamer_on = True
+                a = row.Index - pd.Timedelta(2, "s")
+    return streamer_intervals
 
 
 def _get_streamer_ts_list(df):
-    df = df.loc[df['nvolt'] * df['ncurr'] != 0]
-    df = df.loc[df['stable'] == False]
-    streamer_ts_set = set()
-    for row in df.itertuples(index=True, name='Pandas'):
-        streamer_ts_temp = set([row.Index + i*pd.Timedelta(1, "s") for i in range(-2, 3)])
-        streamer_ts_set = streamer_ts_set.union(streamer_ts_temp)
-    streamer_ts_list = list(streamer_ts_set)
-    streamer_ts_list.sort()
-    return streamer_ts_list
+    df_copy_dict = {}
+    for i in range(-2, 3):
+        df_copy_dict[i] = pd.DataFrame({str(i): df['stable'].copy()})
+        df_copy_dict[i].index = pd.to_datetime(df_copy_dict[i].index).shift(i, freq='S')
+    df_merge = pd.concat([_df for _df in df_copy_dict.values()], axis=1)
+    print(f'df_merge =\n{df_merge}')
+    df_merge['res'] = False
+    print(f'df_merge =\n{df_merge}')
+    df_merge['res'] = df.product(axis=1, skipna=True)
+
+    print(f'df_merge =\n{df_merge}')
+    print(f'returning {df_merge.loc[df_merge["res"]!=True].index}')
+    print(f'with length {len(df_merge.loc[df_merge["res"]!=True].index)}')
+    return df_merge.loc[df_merge['res']>1].index
 
 
 def _get_streamer_intervals_from_ts_list(ts_list):
@@ -30,7 +56,14 @@ def _get_streamer_intervals_from_ts_list(ts_list):
     return streamer_intervals
 
 
-def get_streamer_intervals(df):
+def get_streamer_intervals_new(df):
+    ### the vectorization of this method is tricky
+    ### dataframe rows with no entries (NaN) can be
+    ### streamer or stable, depending on previous
+    ### values
+    t_0 = datetime.now()
     streamer_ts_list = _get_streamer_ts_list(df)
-    return _get_streamer_intervals_from_ts_list(streamer_ts_list)
+    s_i = _get_streamer_intervals_from_ts_list(streamer_ts_list)
+    print(f'get_streamer_intervals took {datetime.now()-t_0} sec')
+    return s_i
 
